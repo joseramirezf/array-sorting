a2f4d7424932b5436b8bcf34cd24c0913daaa2ea
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-07-27 15:45:49 -0600

    Array Sorting

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("\nArray Sorting");
+
+int[] array = new int [5000];
+int randomNumber;
+Random randomNumbers = new Random();
+
+Console.WriteLine("\nRandom List\n");
+
+for (int i = 0; i < 1000; i++)
+ {
+ int numbers = randomNumbers.Next(0, 1000);
+ array[i] = numbers;
+ randomNumber = array[i];
+ Console.Write(randomNumber + " ");
+ }
+
+Console.WriteLine("\n\nSorted List\n");
+Array.Sort(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
